48e333ee080f216981ef164ec438e80769ad17eb
Rahul Rahul <rahul.rahul@kyndryl.com>
2022-06-29 11:00:23 +0200

    Added output text in multithreading example1 file

--- MultiThreading/Example1.py
+++ MultiThreading/Example1.py
@@ -1,8 +1,10 @@
 import time
 import threading
 
+square_arr = []
+
 def square(lst):
-    square_arr = []
+    global square_arr
     for x in lst:
         time.sleep(0.5)
         square_arr.append(x*x)
@@ -16,6 +18,7 @@ def cube(lst):
         cube_arr.append(x*x*x)
 
     print(cube_arr)
+
 
 arr = [1, 2, 3, 4]
 
@@ -34,3 +37,15 @@ t1.join()
 t2.join()
 
 print("Total time to execute : ",time.time()-t)
+print(square_arr)
+
+
+
+'''
+Output:
+[1, 4, 9, 16]
+[1, 8, 27, 64]
+Total time to execute :  2.02390456199646
+[1, 4, 9, 16]
+
+'''
